409fb5b9772d2020619af3b21169c3d6c868a70a
spjustice <sanyok.vergun@gmail.com>
2021-10-15 14:18:47 +0300

    add some content

--- app/js/main.js
+++ app/js/main.js
@@ -2,18 +2,22 @@ $(document).ready(function () {
 
    // select
    $('.js-select-head').on('click', function () {
-      btn = $(this);
-      btn.closest('.js-select').toggleClass('open');
+      var btn = $(this);
+      var block = btn.closest(".js-select");
 
+      if (!block.hasClass('open')) {
+         $('.js-select').removeClass('open');
+      }
+      block.toggleClass('open');
    });
    $('.js-select-body .js-select-item').on('click', function () {
       btn = $(this);
       block = btn.closest('.js-select');
-      // thisText = btn.text()
+      thisText = btn.text()
 
-      btn.closest('.js-select').toggleClass('open');
+      block.toggleClass('open');
       // btn.swap('.js-select-head .js-select-item');
-      // block.find('.js-select-head .lang__item').text(thisText);
+      block.find('.js-select-head p').text(thisText);
    });
 
    $(document).on('mouseup', function (e) {
@@ -42,4 +46,46 @@ $(document).ready(function () {
    // };
    // select end
 
+   // popup
+   $('.btn-popup-rules-of-entrance').on('click', function () {
+      $('.popup-rules-of-entrance').fadeIn();
+   })
+   $('.btn-popup-city-rules').on('click', function () {
+      $('.popup-city-rules').fadeIn();
+   })
+   $('.btn-popup-hotels-rules').on('click', function () {
+      $('.popup-hotels-rules').fadeIn();
+   })
+   $('.btn-popup-public-rules').on('click', function () {
+      $('.popup-public-rules').fadeIn();
+   })
+   $('.btn-popup-info-travel').on('click', function () {
+      $('.popup-info-travel').fadeIn();
+   })
+   $('.btn-popup-certificate').on('click', function () {
+      $('.popup-certificate').fadeIn();
+   })
+   $('.btn-popup-questions').on('click', function () {
+      $('.popup-questions').fadeIn();
+   })
+
+   // страны
+   $('.btn-popup-country-egypt').on('click', function () {
+      $('.popup-country-egypt').fadeIn();
+   })
+   //нажатие вне body
+   $(document).on('click', function (e) {
+      if (!$(e.target).closest(".popup__content").length && !$(e.target).closest(".btn-popup").length) {
+         $('.popup').fadeOut();
+      }
+      e.stopPropagation();
+   });
+   $('.popup-close').on('click', function () {
+      $('.popup').fadeOut();
+   })
+   $('.popup .btn').on('click', function () {
+      $('.popup').fadeOut();
+   })
+   //нажатие вне body
+   //popup end
 });
